0d4afb36f0f45da79afc5ef8e09e0ef91f834ae4
Senrui Chen <641351553@qq.com>
2018-12-04 11:34:22 +0800

    convolution code simulation

--- Simulation.m
+++ Simulation.m
@@ -6,30 +6,40 @@
 %
 %A test program. without Encryption, without Encryption.
 %
-M = 2; 
-N = 8000; %number of bits
+M = 3;
+Rc= 1/3; 
+N = 8001; %number of bits
 
 Pt_list = [];
 Ps_list = [];
-enr_list = [-4:2:16];
-
+enr_list = [-4:1:16];
+ep = 1;%卷积码没法估算误码率
 for enr = enr_list;
     err = 0;
     tot = 0;
     en = 10^(enr/10);
-    P_theo = qfunc(sqrt(2*en));
-    if(P_theo<5*10e-5)
-        break
-    end
-    ep = max(1,round(500/N/P_theo));%确保估计精确度
+    
+    %P_theo = 2/3*qfunc(sqrt(en*3*(1-2^-0.5)));
+    % if(P_theo<5*10e-5)
+        % break
+    % end
+    % ep = max(1,round(500/N/P_theo));%确保估计精确度
+    
     for (ep_cnt = 1:ep)
         message = randi(2,1,N)-1;
-        sstream = bits2syms(message,M,0,1);
-        res = syms2bits(WaveChannel(sstream,M,enr),M,0,1);
+        sstream = bits2syms(message,M,0,2);
+        res = syms2bits(WaveChannel(sstream,M,M,enr),M,0,2);
+        res = res(1:N);%卷积码可能有残留尾巴
         err = err + sum(abs(res - message));  
         tot = tot + N;
     end
-    Pt_list = [Pt_list,P_theo];
+    %Pt_list = [Pt_list,P_theo];
     Ps_list = [Ps_list,err/tot];
-    [enr err/tot P_theo]
+    %[enr err/tot P_theo]
+    [enr err/tot]
+    
+    if(err/tot<3e-4)
+        break
+    end
+    ep = max(ep,round(1000/N/(err/tot)))
 end
